5628e5ce10357d52aab2a013053e6ff4bf53376e
KagareK <KagareK@yandex.ru>
2016-08-04 17:15:22 +0300

    Refactoring part 1

---
+++ src/pdf/PDFBoxUtils.java
@@ -0,0 +1,7 @@
+package pdf;
+
+/**
+ * Created by Igor_Makarychev on 8/4/2016.
+ */
+public class PDFBoxUtils {
+}
--- src/pdf/SplitAndMerge.java
+++ src/pdf/SplitAndMerge.java
@@ -5,7 +5,6 @@ import org.apache.pdfbox.util.Splitter;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 /**
